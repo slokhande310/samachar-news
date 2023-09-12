22be58b821cae96e184100b4cbda189b7153504f
Shubham Lokhande <slokhande310@gmail.com>
2023-09-12 20:46:03 +0530

    converted to fn based component, fixed bugs

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/News.css';
 import NewsItem from './NewsItem';
 import PropTypes from 'prop-types';
@@ -6,79 +6,68 @@ import testImage from '../img/big-s-logo.png'
 import InfiniteScroll from 'react-infinite-scroll-component';
 import NewSpinner from './Spinner';
 
-export default class News extends Component {
+export default function News(props) {
 
-    static defaultProps = {
-        country: 'in'
-    }
-    static propTypes = {
-        country: PropTypes.string,
-        category: PropTypes.string,
-    }
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [totalResults, setTotalResults] = useState(0);
 
-    articles = [];
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            articles: this.articles,
-            page: 1
-        }
-
-    }
-
-    async updateNews() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a34fe631a41b47fd8b73749049e5615e&page=1`;
-        this.setState({ loading: true });
+    const updateNews = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=f4b0f15ab01441179044ee120a956d63&page=1`;
+        setLoading(true);
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({
-            articles: parsedData.articles,
-            totalResults: parsedData.totalResults,
-            loading: false
-        });
+        setArticles(parsedData.articles);
+        setTotalResults(parsedData.totalResults);
+        setLoading(false);
     }
 
-    async componentDidMount() {
-        this.updateNews();
-    }
+    useEffect(() => {
+        updateNews();
+        // eslint-disable-next-line
+    },[]);
 
-    fetchMoreData = async () => {
-        this.setState({
-            page: this.state.page + 1
-        }, () => { this.updateNews() });
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a34fe631a41b47fd8b73749049e5615e&page=${this.state.page}`;
-        this.setState({ loading: true });
+    const fetchMoreData = async () => {
+        setPage(page + 1);
+        let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=f4b0f15ab01441179044ee120a956d63&page=${page+1}`;
+        setLoading(true);
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({
-            articles: this.state.articles.concat(parsedData.articles),
-            totalResults: parsedData.totalResults,
-            loading: false
-        });
+        setArticles(articles.concat(parsedData.articles));
+        setTotalResults(parsedData.totalResults);
+        setLoading(false);
     }
 
-    render() {
-        return (
-            <>
-                <div className="main">
-                    <h2>Top Headlines </h2>
-                    <InfiniteScroll key={'infyScroll'}
-                        dataLength={this.state.articles.length}
-                        next={this.fetchMoreData}
-                        hasMore={this.state.articles.length !== this.state.totalResults}
-                        loader={this.state.loading && <NewSpinner />}
-                    >
-                        <div className="row">
-                            {this.state.articles.map((element) => {
-                                return <div className="column" key={element.url}>
-                                    <NewsItem title={element.title ? element.title.slice(0, 70) : ""} description={element.description ? element.description.slice(0, 150) : "Read More on website"} author={element.author ? element.author : 'Unknown'} date={element.publishedAt} newsUrl={element.url} imageUrl={element.urlToImage ? element.urlToImage : testImage} />
-                                </div>
-                            })}
-                        </div>
-                    </InfiniteScroll>
-                </div>
-            </>
-        )
-    }
+    return (
+        <>
+            <div className="main">
+                <h2>Top Headlines </h2>
+                {loading && <NewSpinner />}
+                <InfiniteScroll key={'infyScroll'}
+                    dataLength={articles.length}
+                    next={fetchMoreData}
+                    hasMore={articles.length !== totalResults}
+                    loader={<NewSpinner />}
+                >
+                    <div className="row">
+                        {articles.map((element) => {
+                            return <div className="column" key={element.url}>
+                                <NewsItem title={element.title ? element.title.slice(0, 70) : ""} description={element.description ? element.description.slice(0, 150) : "Read More on website"} author={element.author ? element.author : 'Unknown'} date={element.publishedAt} newsUrl={element.url} imageUrl={element.urlToImage ? element.urlToImage : testImage} />
+                            </div>
+                        })}
+                    </div>
+                </InfiniteScroll>
+            </div>
+        </>
+    )
+
+}
+
+News.defaultProps = {
+    country: 'in'
+}
+News.propTypes = {
+    country: PropTypes.string,
+    category: PropTypes.string,
 }
