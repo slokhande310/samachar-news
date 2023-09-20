89c686e4f218aa69f3f361f3295435d2ef8ef7a9
Shubham Lokhande <slokhande310@gmail.com>
2023-09-20 14:26:28 +0530

    replaced vanilla js with usestate for ham menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Navbar.css'
 import { Link } from 'react-router-dom'
 
-const navbar = document.querySelector('.navbar');
-const navbarItems = document.querySelector('.navbar-items');
-const listItems = document.querySelectorAll('.list-items');
-const links = document.querySelectorAll('.links');
-
 function Navbar() {
 
+    const [isActive, setActive] = useState('false');
+
     const toggleMenu = () => {
-        navbar.classList.toggle('active');
-        navbarItems.classList.toggle('active');
-        listItems.forEach((e) => {
-            e.classList.toggle('active');
-        });
-        links.forEach((e) => {
-            e.classList.toggle('active');
-        });
-    }
+        setActive(!isActive)
+    };
 
     return (
         <>
-            <div className="navbar">
+            <div className={`navbar ${isActive ? '' : 'active'}`}>
                 <li className='ham' onClick={toggleMenu}><i className="fa-solid fa-bars" ></i></li>
-                <div className="navbar-items  ">
-                    <li className='list-items'><Link className='links' to="/">Home</Link></li>
-                    <li className='list-items'><Link className='links' to="/world">World</Link></li>
-                    <li className='list-items'><Link className='links' to="/business">Business</Link></li>
-                    <li className='list-items'><Link className='links' to="/sports">Sports</Link></li>
-                    <li className='list-items'><Link className='links' to="/technology">Technology</Link></li>
-                    <li className='list-items'><Link className='links' to="/science">Science</Link></li>
-                    <li className='list-items'><Link className='links' to="/entertainment">Entertainment</Link></li>
-                    <li className='list-items'><Link className='links' to="/health">Health</Link></li>
+                <div className={`navbar-items ${isActive ? '' : 'active'}`}>
+                    <li className='list-items'><Link className={`links ${isActive ? '' : 'active'}`} to="/">Home</Link></li>
+                    <li className='list-items'><Link className={`links ${isActive ? '' : 'active'}`} to="/world">World</Link></li>
+                    <li className='list-items'><Link className={`links ${isActive ? '' : 'active'}`} to="/business">Business</Link></li>
+                    <li className='list-items'><Link className={`links ${isActive ? '' : 'active'}`} to="/sports">Sports</Link></li>
+                    <li className='list-items'><Link className={`links ${isActive ? '' : 'active'}`} to="/technology">Technology</Link></li>
+                    <li className='list-items'><Link className={`links ${isActive ? '' : 'active'}`} to="/science">Science</Link></li>
+                    <li className='list-items'><Link className={`links ${isActive ? '' : 'active'}`} to="/entertainment">Entertainment</Link></li>
+                    <li className='list-items'><Link className={`links ${isActive ? '' : 'active'}`} to="/health">Health</Link></li>
                 </div>
 
             </div>
